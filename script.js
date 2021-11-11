15cf94b1baaa01e6af420d931a1d068b12600a32
Nicolas Gonzalez <nicogonzalez2000@gmail.com>
2021-11-11 00:01:25 -0300

    Cambios

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let num = parseInt(prompt("Ingrese un número: "));
 
 let iva = num*(1.22);
 
-let resto = (iva-resto);
+let resto = iva-num;
 
 alert("El iva es " + resto);
 alert("El precio con IVA incluido es: "+ iva)
